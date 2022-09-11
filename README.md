d655b0724d89761cd1bb244e145e805991f0194c
Hakan FIRAT <hknfrt@protonmail.com>
2022-09-11 20:26:47 +0300

    day 1

---
+++ README.md
@@ -0,0 +1,8 @@
+# 30DaysOfJS
+
+30 Days Of JavaScript: Introduction
+
+I deciding to participate in 30 days of JavaScript programming challenge. 
+In this challenge I hope, I would learn everything I need to be a JavaScript programmer, and in general, the whole concept of programming. If you want to learn it with me, don't hesiteiate and join me.
+
+I am thankful to Asabeneh S. Yetayeh , and also if you wanna, you can visit his github profile click here @Asabeneh
---
+++ src/Day_01/main.js
@@ -0,0 +1,100 @@
+console.log('Welcome to 30DaysOfJavaScript Challenge')
+
+//Day 1: Exercises
+
+// 1- Write a single line comment which says, comments can make code readable
+
+// Comments can make code readable
+
+// 2- Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+// Welcome to 30DaysOfJavaScript
+
+// 3- Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+/*
+
+Comments can make code readable, easy to reuse and informative
+
+*/
+
+// 4- Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+/*
+
+let stringType = "Hakan"
+let booleanType = false
+let undifinedType
+let nullType = null
+
+*/
+
+// 5- Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+/* 
+
+console.log(typeof stringType)
+console.log(typeof booleanType)
+console.log(typeof undifinedType)
+console.log(typeof nullType)
+
+*/
+
+// 6- Declare four variables without assigning values
+
+/*
+
+let firstName
+let lastName
+let age
+let isMarried
+
+*/
+
+// 7- Declare four variables with assigning values 
+
+/* 
+
+let firstName = 'Hakan'
+let lastName = 'FIRAT'
+let age = 34
+let isMarried = true 
+
+*/
+
+// 8- Declare variables to store your first name, last name, marital status, country and age in multiple lines 
+
+/* 
+
+let firstName = 'Hakan'
+let lastName = 'FIRAT'
+let age = 34
+let isMarried = true 
+let country = 'Turkey'
+
+*/
+
+// 9- Declare variables to store your first name, last name, marital status, country and age in a single line
+
+/* 
+
+let firstName = 'Hakan',
+lastName = 'FIRAT',
+age = 34,
+isMarried = true, 
+country =turkey
+
+*/
+
+
+// 10- Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+let myAge = 34
+
+let yourAge = 30
+
+console.log("I am "+myAge+" "+"years old")
+console.log("You are "+yourAge+" "+"years old" )
+
+
+
