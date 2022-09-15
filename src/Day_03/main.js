a162a381712b5320c9e4478596170e119536ebce
hakan <hknfrt@protonmail.com>
2022-09-15 08:40:09 +0300

    day 3

--- src/Day_03/main.js
+++ src/Day_03/main.js
@@ -224,17 +224,15 @@ let times = new Date()
 // alert('The circumference of a circle is: ' + calcCircumferenceOfCircle)
 
 // 5- Calculate the slope, x-intercept and y-intercept of y = 2x -2
-let y,x
-y = '2*x-2'
-let getX = y.includes('x')
-getX == true
-? console.log('This is slope: ' + y[0]): console.log('The slope is 0')
 
-
-
-
+// let y,x
+// y = '2*x-2'
+// let getX = y.includes('x')
+// getX == true
+// ? console.log('This is slope: ' + y[0]): console.log('The slope is 0')
 
 // 6- Slope is m = (y2-y1)/(x2-x1). Find the slope between point (2, 2) and point(6,10)
+
 
 // 7- Compare the slope of above two questions.
 
