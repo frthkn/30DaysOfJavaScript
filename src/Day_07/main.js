367d439ea18bd744729416727a63acd776e57eda
hakan <hknfrt@protonmail.com>
2022-09-23 17:12:38 +0300

    day 7

--- src/Day_07/main.js
+++ src/Day_07/main.js
@@ -168,3 +168,102 @@
 
 // console.log(findMax(-150,-2,-110))
 
+// Exercises: Level 2
+
+
+
+// 1- Linear equation is calculated as follows: ax + by + c = 0. Write a function which calculates value of a linear equation, solveLinEquation.
+
+// function solveLinEquation(a,b,c){
+//     let solve = a+'x'+'+'+b+'y'+'+'+c+' = '+0
+//     if(a==1 && b ==1){
+//         return 'x+y+'+c+' = '+0
+//     }else if( a ==1){
+//         return 'x+'+b+'y+'+c+' = '+0
+//     }else if(b==1){
+//         return a+'x+y+'+c+' = '+0
+//     }
+//     return solve
+// }
+// console.log(solveLinEquation(1,1,5))
+// '\n'
+// console.log(solveLinEquation(1,3,5))
+// '\n'
+// console.log(solveLinEquation(4,1,5))
+
+// 2- Quadratic equation is calculated as follows: ax2 + bx + c = 0. Write a function which calculates value or values of a quadratic equation, solveQuadEquation.
+
+// function solveQuadEquation(a,b,c){
+//     let sqrt = Math.sqrt(b*b-4*a*c)
+//     let solve1 = ((-b+sqrt)/2*a)
+//     let solve2 = ((-b-sqrt)/2*a)
+//     let solve = `{${solve1}, ${solve2}}`
+//     if(a == 0){
+//         console.log(" a can not be equal zero(0)" )
+//         return `{0}`
+//     } else if(solve1 === solve2) {
+//         return `{${solve1}}`
+//     }
+//     return solve
+    
+// }
+// console.log(solveQuadEquation(0,-1,-2))
+// console.log(solveQuadEquation(1,-1,-2))
+// console.log(solveQuadEquation(1, 4, 4))
+// console.log(solveQuadEquation(1, 7, 12))
+
+// 3- Declare a function name printArray. It takes array as a parameter and it prints out each value of the array.
+
+// Write a function name showDateTime which shows time in this format: 08/01/2020 04:08 using the Date object.
+
+// showDateTime()
+// 08/01/2020 04:08
+
+// Declare a function name swapValues. This function swaps value of x to y.
+
+// swapValues(3, 4) // x => 4, y=>3
+// swapValues(4, 5) // x = 5, y = 4
+
+// Declare a function name reverseArray. It takes array as a parameter and it returns the reverse of the array (don't use method).
+
+// console.log(reverseArray([1, 2, 3, 4, 5]))
+// //[5, 4, 3, 2, 1]
+// console.log(reverseArray(['A', 'B', 'C']))
+// //['C', 'B', 'A']
+
+// Declare a function name capitalizeArray. It takes array as a parameter and it returns the - capitalizedarray.
+
+// Declare a function name addItem. It takes an item parameter and it returns an array after adding the item
+
+// Declare a function name removeItem. It takes an index parameter and it returns an array after removing an item
+
+// Declare a function name sumOfNumbers. It takes a number parameter and it adds all the numbers in that range.
+
+// Declare a function name sumOfOdds. It takes a number parameter and it adds all the odd numbers in that - range.
+
+// Declare a function name sumOfEven. It takes a number parameter and it adds all the even numbers in that - range.
+
+// Declare a function name evensAndOdds . It takes a positive integer as parameter and it counts number of evens and odds in the number.
+
+// evensAndOdds(100);
+// The number of odds are 50.
+// The number of evens are 51.
+
+// Write a function which takes any number of arguments and return the sum of the arguments
+
+// sum(1, 2, 3) // -> 6
+// sum(1, 2, 3, 4) // -> 10
+
+// Writ a function which generates a randomUserIp.
+
+// Write a function which generates a randomMacAddress
+
+// Declare a function name randomHexaNumberGenerator. When this function is called it generates a random hexadecimal number. The function return the hexadecimal number.
+
+// console.log(randomHexaNumberGenerator());
+// '#ee33df'
+
+// Declare a function name userIdGenerator. When this function is called it generates seven character id. The function return the id.
+
+// console.log(userIdGenerator());
+// 41XTDbE
