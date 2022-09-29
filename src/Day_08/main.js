ed413f7ebc732b5929f8002125e32fa4b3e6b429
hakan <hknfrt@protonmail.com>
2022-09-29 22:57:22 +0300

    day 8

--- src/Day_08/main.js
+++ src/Day_08/main.js
@@ -1,5 +1,58 @@
 // Day 8
 
+const users = {
+    Alex: {
+      email: 'alex@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      age: 20,
+      isLoggedIn: false,
+      points: 30
+    },
+    Asab: {
+      email: 'asab@asab.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 25,
+      isLoggedIn: false,
+      points: 50
+    },
+    Brook: {
+      email: 'daniel@daniel.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+      age: 30,
+      isLoggedIn: true,
+      points: 50
+    },
+    Daniel: {
+      email: 'daniel@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    John: {
+      email: 'john@john.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+      age: 20,
+      isLoggedIn: true,
+      points: 50
+    },
+    Thomas: {
+      email: 'thomas@thomas.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    Paul: {
+      email: 'paul@paul.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    }
+  }
+  
+
 // Exercises: Level 1
 
 
@@ -13,15 +66,15 @@
 
 // 3- Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
 
-const dog = {name : 'Lucky',
-legs : 4,
-color : 'black',
-age : 5,
-bark : function(){
-    return 'woof woof'
-}
+// const dog = {name : 'Lucky',
+// legs : 4,
+// color : 'black',
+// age : 5,
+// bark : function(){
+//     return 'woof woof'
+// }
 
-}
+// }
 // 4- Get name, legs, color, age and bark value from the dog object
 
 // console.log(dog)
@@ -37,9 +90,6 @@ bark : function(){
 
 // Exercises: Level 2
 
-
 // 1- Find the person who has many skills in the users object.
-
-
 
 // Count logged in users, count users having greater than equal to 50 points from the following object.
