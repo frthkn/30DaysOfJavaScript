2b1b457f64349c1b7a830cb59c3db4cba277aae3
hakan <hknfrt@protonmail.com>
2022-09-29 11:07:27 +0300

    day 8

---
+++ src/Day_08/main.js
@@ -0,0 +1,12 @@
+// Day 8
+
+// Exercises: Level 1
+
+
+// Create an empty object called dog
+// Print the the dog object on the console
+// Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
+// Get name, legs, color, age and bark value from the dog object
+// Set new properties the dog object: breed, getDogInfo
+
+// Exercises: Level 2
