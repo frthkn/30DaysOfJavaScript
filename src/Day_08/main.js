eb9344a38b5d25555e40535b7a6b44cb8e8315ab
hakan <hknfrt@protonmail.com>
2022-09-29 14:44:26 +0300

    day 8

--- src/Day_08/main.js
+++ src/Day_08/main.js
@@ -5,7 +5,7 @@
 
 // 1- Create an empty object called dog
 
-const dog = {}
+// const dog = {}
 
 // 2- Print the the dog object on the console
 
@@ -13,24 +13,33 @@ const dog = {}
 
 // 3- Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
 
-dog.name = 'Lucky'
-dog.legs = 4
-dog.color = 'black'
-dog.age = 5
-dog.bark = function(){
+const dog = {name : 'Lucky',
+legs : 4,
+color : 'black',
+age : 5,
+bark : function(){
     return 'woof woof'
 }
 
-
+}
 // 4- Get name, legs, color, age and bark value from the dog object
 
 // console.log(dog)
 
 // 5- Set new properties the dog object: breed, getDogInfo
 
+// dog.breed = 'Dingo'
+// dog.getDogInfo = function(){
+//     return `Dog name: ${dog.name}, breed: ${dog.breed}, age: ${dog.age}, color: ${dog.color} and legs: ${dog.legs}`
+
+// }
+// console.log(dog.getDogInfo())
+
 // Exercises: Level 2
 
 
-// Find the person who has many skills in the users object.
+// 1- Find the person who has many skills in the users object.
+
+
 
 // Count logged in users, count users having greater than equal to 50 points from the following object.
