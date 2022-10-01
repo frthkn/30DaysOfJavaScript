86d957fe7dfb4bed9298e6c27a96d53dd703e92d
hakan <hknfrt@protonmail.com>
2022-10-01 21:53:23 +0300

    day 8

--- src/Day_08/main.js
+++ src/Day_08/main.js
@@ -52,48 +52,48 @@
 //     }
 //   }
   
-const users = [
-  {
-      _id: 'ab12ex',
-      username: 'Alex',
-      email: 'alex@alex.com',
-      password: '123123',
-      createdAt:'08/01/2020 9:00 AM',
-      isLoggedIn: false
-  },
-  {
-      _id: 'fg12cy',
-      username: 'Asab',
-      email: 'asab@asab.com',
-      password: '123456',
-      createdAt:'08/01/2020 9:30 AM',
-      isLoggedIn: true
-  },
-  {
-      _id: 'zwf8md',
-      username: 'Brook',
-      email: 'brook@brook.com',
-      password: '123111',
-      createdAt:'08/01/2020 9:45 AM',
-      isLoggedIn: true
-  },
-  {
-      _id: 'eefamr',
-      username: 'Martha',
-      email: 'martha@martha.com',
-      password: '123222',
-      createdAt:'08/01/2020 9:50 AM',
-      isLoggedIn: false
-  },
-  {
-      _id: 'ghderc',
-      username: 'Thomas',
-      email: 'thomas@thomas.com',
-      password: '123333',
-      createdAt:'08/01/2020 10:00 AM',
-      isLoggedIn: false
-  }
-  ];
+// const users = [
+//   {
+//       _id: 'ab12ex',
+//       username: 'Alex',
+//       email: 'alex@alex.com',
+//       password: '123123',
+//       createdAt:'08/01/2020 9:00 AM',
+//       isLoggedIn: false
+//   },
+//   {
+//       _id: 'fg12cy',
+//       username: 'Asab',
+//       email: 'asab@asab.com',
+//       password: '123456',
+//       createdAt:'08/01/2020 9:30 AM',
+//       isLoggedIn: true
+//   },
+//   {
+//       _id: 'zwf8md',
+//       username: 'Brook',
+//       email: 'brook@brook.com',
+//       password: '123111',
+//       createdAt:'08/01/2020 9:45 AM',
+//       isLoggedIn: true
+//   },
+//   {
+//       _id: 'eefamr',
+//       username: 'Martha',
+//       email: 'martha@martha.com',
+//       password: '123222',
+//       createdAt:'08/01/2020 9:50 AM',
+//       isLoggedIn: false
+//   },
+//   {
+//       _id: 'ghderc',
+//       username: 'Thomas',
+//       email: 'thomas@thomas.com',
+//       password: '123333',
+//       createdAt:'08/01/2020 10:00 AM',
+//       isLoggedIn: false
+//   }
+//   ];
 
   const products = [
 {
@@ -336,7 +336,7 @@ const users = [
 //       if((users[key].username == n) && (users[key].password == p )) {
 //         return users[key]
 //       }else if(!(users[key].username == n) && (users[key].password == p )){
-//         return `Try again`
+//         return `Username or password is wrong`
 //       }
 //   }
 //   return `Try again`
@@ -346,6 +346,18 @@ const users = [
 
 // 3- The products array has three elements and each of them has six properties. 
 // 3.a - Create a function called rateProduct which rates the product 
+
+
+function rateProduct(){
+  let arr = []
+  arr.push(products[0].ratings[0].rate)
+  arr.push(products[0].ratings[1].rate)
+
+  return arr
+}
+
+console.log(rateProduct())
+
 
 
 
