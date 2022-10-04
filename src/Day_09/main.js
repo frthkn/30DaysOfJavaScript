2803aadadb4902902e437f945f4f25b95290f6a0
hakan <hknfrt@protonmail.com>
2022-10-04 17:04:08 +0300

    day 9

--- src/Day_09/main.js
+++ src/Day_09/main.js
@@ -1,8 +1,11 @@
+
 // Day 9
+
+import {countries} from './countries.js'
 
 // Exercises: Level 1
 
-const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand', 'Turkey']
+// const countries = ['Finland', 'Sweden', 'Denmark', 'Norway', 'IceLand', 'Turkey']
 const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook', 'Hakan']
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const products = [
@@ -171,10 +174,35 @@ const callback = (n) => {
 
 // 1- Find the total price of products by chaining two or more array iterators(eg. arr.map(callback).filter(callback).reduce(callback))
 
+// let assingZeroToEmptyPrice = products.filter((a)=> {if((a.price == '') || (a.price == ' ')){a.price =0}})
+// let totalPrice = products.map((item => item.price)).reduce((first, end) => (first+end))
+// console.log(totalPrice)
 
+// 2- Find the sum of price of products using only reduce reduce(callback))
 
-// Find the sum of price of products using only reduce reduce(callback))
-// Declare a function called categorizeCountries which returns an array of countries which have some common pattern(you find the countries array in this repository as countries.js(eg 'land', 'ia', 'island','stan')).
+// let totalPriceWithOnlyReduce = products.reduce((item1,item2,a)=> {
+//   if((products[a].price == '') || (products[a].price == ' ')){
+//     products[a].price = 0
+//   }
+// })
+// let sumPrice = products.reduce((a,b)=> b.price+a,0)
+// console.log(sumPrice)
+
+// 3- Declare a function called categorizeCountries which returns an array of countries which have some common pattern(you find the countries array in this repository as countries.js(eg 'land', 'ia', 'island','stan')).
+
+console.log(countries)
+
+function categorizeCountries(){
+  for (const i in countries) {
+    if(i.length == 7){
+      return i
+    }
+  }
+
+}
+
+console.log(categorizeCountries)
+
 // Create a function which return an array of objects, which is the letter and the number of times the letter use to start with a name of a country.
 // Declare a getFirstTenCountries function and return an array of ten countries. Use different functional programming to work on the countries.js array
 // Declare a getLastTenCountries function which which returns the last ten countries in the countries array.
