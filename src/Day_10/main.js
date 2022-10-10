f07f122d44312baf1be70c1b57d97bccac5c5526
hakan <hknfrt@protonmail.com>
2022-10-10 10:40:19 +0300

    day 10

--- src/Day_10/main.js
+++ src/Day_10/main.js
@@ -5,3 +5,75 @@
 const a = [4, 5, 8, 9]
 const b = [3, 4, 5, 7]
 const countries = ['Finland', 'Sweden', 'Norway']
+
+
+// 1- create an empty set
+
+const emptySet = new Set()
+
+
+
+// 2- Create a set containing 0 to 10 using loop
+
+const zeroToTenArr = [0,1,2,3,4,5,6,7,8,9,10]
+const zeroToTenSet = new Set()
+for (const i of zeroToTenArr) {
+    zeroToTenSet.add(i)
+}
+
+// 3- Remove an element from a set
+
+zeroToTenSet.delete(0)
+
+// 4- Clear a set
+
+zeroToTenSet.clear()
+
+// 5- Create a set of 5 string elements from array
+
+const stringForSet = new Set()
+stringForSet.add('one')
+stringForSet.add('two')
+stringForSet.add('three')
+stringForSet.add('four')
+stringForSet.add('five')
+
+
+// 6- Create a map of countries and number of characters of a country
+
+
+// Exercises:Level 2
+
+// Find a union b
+// Find a intersection b
+// Find a with b
+
+// Exercises:Level 3
+
+// How many languages are there in the countries object file.
+
+// *** Use the countries data to find the 10 most spoken languages:
+
+   // Your output should look like this
+//    console.log(mostSpokenLanguages(countries, 10))
+//    [
+//      { English: 91 },
+//      { French: 45 },
+//      { Arabic: 25 },
+//      { Spanish: 24 },
+//      { Russian: 9 },
+//      { Portuguese: 9 },
+//      { Dutch: 8 },
+//      { German: 7 },
+//      { Chinese: 5 },
+//      { Swahili: 4 },
+//      { Serbian: 4 }
+//    ]
+
+  // Your output should look like this
+//   console.log(mostSpokenLanguages(countries, 3))
+//   [
+//   {English:91},
+//   {French:45},
+//   {Arabic:25}
+//   ]
