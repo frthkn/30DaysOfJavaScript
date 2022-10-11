c3987c331196dc095503c05e2558f5a6ec692550
hakan <hknfrt@protonmail.com>
2022-10-11 18:23:57 +0300

    day 11

--- src/Day_11/main.js
+++ src/Day_11/main.js
@@ -75,3 +75,97 @@ const {width: a, height: b, area: c, perimeter: {d = 80}} = rectangle
 // console.log(a) // a(width) = 20
 
 // console.log(d) // 80
+
+
+// Exercises: Level 2
+
+
+// 1- Iterate through the users array and get all the keys of the object using destructuring
+
+// Find the persons who have less than two skills
+
+
+// Exercises: Level 3
+
+
+// Destructure the countries object print name, capital, population and languages of all countries
+// A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
+
+const students = [
+    ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+    ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+  ]
+
+console.log(convertArrayToObject(students))
+[
+  {
+    name: 'David',
+    skills: ['HTM','CSS','JS','React'],
+    scores: [98,85,90,95]
+  },
+  {
+    name: 'John',
+    skills: ['HTM','CSS','JS','React'],
+    scores: [85, 80,85,80]
+  }
+]
+
+
+// Copy the student object to newStudent without mutating the original object. In the new object add the following ?
+
+
+// Add Bootstrap with level 8 to the front end skill sets
+// Add Express with level 9 to the back end skill sets
+// Add SQL with level 8 to the data base skill sets
+// Add SQL without level to the data science skill sets
+
+const student = {
+    name: 'David',
+    age: 25,
+    skills: {
+      frontEnd: [
+        { skill: 'HTML', level: 10 },
+        { skill: 'CSS', level: 8 },
+        { skill: 'JS', level: 8 },
+        { skill: 'React', level: 9 }
+      ],
+      backEnd: [
+        { skill: 'Node',level: 7 },
+        { skill: 'GraphQL', level: 8 },
+      ],
+      dataBase:[
+        { skill: 'MongoDB', level: 7.5 },
+      ],
+      dataScience:['Python', 'R', 'D3.js']
+    }
+  }
+
+
+//   The copied object output should look like this:
+
+
+// {
+//     name: 'David',
+//     age: 25,
+//     skills: {
+//       frontEnd: [
+//         {skill: 'HTML',level: 10},
+//         {skill: 'CSS',level: 8},
+//         {skill: 'JS',level: 8},
+//         {skill: 'React',level: 9},
+//         {skill: 'BootStrap',level: 8}
+//       ],
+//       backEnd: [
+//         {skill: 'Node',level: 7},
+//         {skill: 'GraphQL',level: 8},
+//         {skill: 'Express',level: 9}
+//       ],
+//       dataBase: [
+//         { skill: 'MongoDB',level: 7.5},
+//         { skill: 'SQL',level: 8}
+//       ],
+//       dataScience: ['Python','R','D3.js','SQL']
+//     }
+//   }
+
+
