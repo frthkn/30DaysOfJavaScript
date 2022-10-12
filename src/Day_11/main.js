926db830fd92ac02d90730ff3e0181e7de3c607c
hakan <hknfrt@protonmail.com>
2022-10-12 18:05:49 +0300

    day 11

--- src/Day_11/main.js
+++ src/Day_11/main.js
@@ -106,11 +106,18 @@ for (const {area: Area,flag: Flag, region:Region} of countries_data) {
 
 // 2- A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
 
-const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85],[ 90, 95]]
-const [name = student[0], skills = student[1], jsScore = student[2], reactScore = student[3]] = student
+const person = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85],[ 90, 95]]
+const [name = person[0], skills = person[1], jsScore = person[2], reactScore = person[3]] = person
 //console.log(name, skills, jsScore, reactScore)
 
-// 3- Copy the student object to newStudent without mutating the original object. In the new object add the following ?
+// Write a function called convertArrayToObject which can convert the array to a structure object.
+
+const students = [
+  ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
+  ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
+]
+
+
 
 //console.log(convertArrayToObject(students))
 // [
@@ -126,12 +133,14 @@ const [name = student[0], skills = student[1], jsScore = student[2], reactScore 
 //   }
 // ]
 
+
+// 4- Copy the student object to newStudent without mutating the original object. In the new object add the following ?
 // Add Bootstrap with level 8 to the front end skill sets
 // Add Express with level 9 to the back end skill sets
 // Add SQL with level 8 to the data base skill sets
 // Add SQL without level to the data science skill sets
 
-const students = {
+const student = {
     name: 'David',
     age: 25,
     skills: {
