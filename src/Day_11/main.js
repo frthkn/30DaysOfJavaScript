7836966b1370e0ea6585d916db1819c95469296d
hakan <hknfrt@protonmail.com>
2022-10-12 18:01:35 +0300

    day 11

--- src/Day_11/main.js
+++ src/Day_11/main.js
@@ -57,7 +57,7 @@ const users = [
 }
 ]
 
-console.log(countries_data)
+
 
 // 1- Destructure and assign the elements of constants array to e, pi, gravity, humanBodyTemp, waterBoilingTemp.
 
@@ -99,37 +99,39 @@ const personsHaveLessThanTwoSkills = users.filter((el => el.skills.length < 2))
 
 // 1- Destructure the countries object print name, capital, population and languages of all countries
 
-// A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
+for (const {area: Area,flag: Flag, region:Region} of countries_data) {
+  //console.log(`Flag: ${Flag}, Region: ${Region}, Area: ${Area}`)
+  
+}
 
-const students = [
-    ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
-    ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
-  ]
+// 2- A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
+
+const student = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85],[ 90, 95]]
+const [name = student[0], skills = student[1], jsScore = student[2], reactScore = student[3]] = student
+//console.log(name, skills, jsScore, reactScore)
+
+// 3- Copy the student object to newStudent without mutating the original object. In the new object add the following ?
 
 //console.log(convertArrayToObject(students))
-[
-  {
-    name: 'David',
-    skills: ['HTM','CSS','JS','React'],
-    scores: [98,85,90,95]
-  },
-  {
-    name: 'John',
-    skills: ['HTM','CSS','JS','React'],
-    scores: [85, 80,85,80]
-  }
-]
-
-
-// Copy the student object to newStudent without mutating the original object. In the new object add the following ?
-
+// [
+//   {
+//     name: 'David',
+//     skills: ['HTM','CSS','JS','React'],
+//     scores: [98,85,90,95]
+//   },
+//   {
+//     name: 'John',
+//     skills: ['HTM','CSS','JS','React'],
+//     scores: [85, 80,85,80]
+//   }
+// ]
 
 // Add Bootstrap with level 8 to the front end skill sets
 // Add Express with level 9 to the back end skill sets
 // Add SQL with level 8 to the data base skill sets
 // Add SQL without level to the data science skill sets
 
-const student = {
+const students = {
     name: 'David',
     age: 25,
     skills: {
