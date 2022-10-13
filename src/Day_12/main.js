57c8ef09acb65516516f8a0f2f799ed5f26a457d
hakan <hknfrt@protonmail.com>
2022-10-13 12:28:32 +0300

    day 12

--- src/Day_12/main.js
+++ src/Day_12/main.js
@@ -6,9 +6,15 @@
 // 1- Calculate the total annual income of the person from the following text. ‘He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.’
 
 const text = 'He earns 4000 euro from salary per month, 10000 euro annual bonus, 5500 euro online courses per month.'
+const totalAnnualIncome = text.match(/\d+/g).map((el => +(el))).reduce((acc, prev) =>  acc+prev ,0)
 
-const getNumber = text.match(/\d/g)
-console.log(getNumber)
+//console.log('Total annual income of the person: ',totalAnnualIncome, 'Euro')
 
 
 // 2- The position of some particles on the horizontal x-axis -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers and find the distance between the two furthest particles.
+
+const points = [4,-4,0,-3,-1,-12,8]
+const sortPoints = points.sort((x,y) => {return x-y})
+const distance = Math.abs(sortPoints[0])+sortPoints[sortPoints.length-1]
+console.log(distance)
+
