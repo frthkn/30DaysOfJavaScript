1a967bf3444e987294849e981b5194d99f771934
hakan <hknfrt@protonmail.com>
2022-10-13 08:42:56 +0300

    day 11

--- src/Day_11/main.js
+++ src/Day_11/main.js
@@ -107,32 +107,26 @@ for (const {area: Area,flag: Flag, region:Region} of countries_data) {
 // 2- A junior developer structure student name, skills and score in array of arrays which may not easy to read. Destructure the following array name to name, skills array to skills, scores array to scores, JavaScript score to jsScore and React score to reactScore variable in one line.
 
 const person = ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85],[ 90, 95]]
-const [name = person[0], skills = person[1], jsScore = person[2], reactScore = person[3]] = person
+//const [name = person[0], skills = person[1], jsScore = person[2], reactScore = person[3]] = person
 //console.log(name, skills, jsScore, reactScore)
 
-// Write a function called convertArrayToObject which can convert the array to a structure object.
+// 3- Write a function called convertArrayToObject which can convert the array to a structure object.
+
+
 
 const students = [
   ['David', ['HTM', 'CSS', 'JS', 'React'], [98, 85, 90, 95]],
   ['John', ['HTM', 'CSS', 'JS', 'React'], [85, 80, 85, 80]]
 ]
 
+function convertArrayToObject(arr){
+  const [[name, skills, scores]] = students
+  const obj = students.map(( key, value) => ( {name, skills, scores} ));
+  return obj
+}
 
 
-//console.log(convertArrayToObject(students))
-// [
-//   {
-//     name: 'David',
-//     skills: ['HTM','CSS','JS','React'],
-//     scores: [98,85,90,95]
-//   },
-//   {
-//     name: 'John',
-//     skills: ['HTM','CSS','JS','React'],
-//     scores: [85, 80,85,80]
-//   }
-// ]
-
+console.log(convertArrayToObject(students))
 
 // 4- Copy the student object to newStudent without mutating the original object. In the new object add the following ?
 // Add Bootstrap with level 8 to the front end skill sets
