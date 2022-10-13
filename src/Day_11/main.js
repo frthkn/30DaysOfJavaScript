dd440d3a8f3c5bda465d8b797cd68f62baa7ef4f
hakan <hknfrt@protonmail.com>
2022-10-13 08:43:31 +0300

    day 11

--- src/Day_11/main.js
+++ src/Day_11/main.js
@@ -125,10 +125,12 @@ function convertArrayToObject(arr){
   return obj
 }
 
-
-console.log(convertArrayToObject(students))
+// console.log(convertArrayToObject(students))
 
 // 4- Copy the student object to newStudent without mutating the original object. In the new object add the following ?
+
+
+
 // Add Bootstrap with level 8 to the front end skill sets
 // Add Express with level 9 to the back end skill sets
 // Add SQL with level 8 to the data base skill sets
