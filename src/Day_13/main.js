97b9a4250021401c20fc5a946cc9cdc1c577098e
hakan <hknfrt@protonmail.com>
2022-10-14 12:12:37 +0300

    day 13

--- src/Day_13/main.js
+++ src/Day_13/main.js
@@ -1,10 +1,26 @@
 // Day 13
 
+import {countries_data} from '/src/Data/data.js'
+
 // Exercises: Level 1
 
 
 // 1- Display the countries array as a table
 
+const countries = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo']
+  ]
+
+// console.table(countries)
 
 // 2- Display the countries object as a table
+
+//console.table(countries_data)
+
 // 3- Use console.group() to group logs
+
+
+// Exercises: Level 2
+
