10ffd92d1f6e8ce18fb7129c129a9c82369f8523
hakan <hknfrt@protonmail.com>
2022-10-14 12:34:22 +0300

    day 13

--- src/Day_13/main.js
+++ src/Day_13/main.js
@@ -43,3 +43,36 @@ console.error('This is an error message')
 // Exercises: Level 3
 
 // 1- Check the speed difference among the following loops: while, for, for of, forEach
+
+console.time('While loop')
+let k =0
+while (k<countries.length) {
+    console.log(countries[k][0], countries[k][1])
+    k++
+} 
+console.timeEnd('While loop')
+
+console.log('\n')
+
+console.time('For loop')
+for (let i = 0; i < countries.length; i++) {
+    console.log(countries[i][0], countries[i][1])
+  }
+console.timeEnd('For loop')
+
+console.log('\n')
+
+
+console.time('For of loop')
+for (const [name, city] of countries) {
+  console.log(name, city)
+}
+console.timeEnd('For of loop')
+
+console.log('\n')
+
+console.time('ForEach loop')
+countries.forEach(([name, city]) => {
+  console.log(name, city)
+})
+console.timeEnd('ForEach loop')
