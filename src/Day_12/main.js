c8ff61beb41553d50b2fc0d704d05302e29c96f1
hakan <hknfrt@protonmail.com>
2022-10-14 11:22:13 +0300

    day 12

--- src/Day_12/main.js
+++ src/Day_12/main.js
@@ -71,7 +71,7 @@ function tenMostFrequentWords(paragraph ,num){
 
 const sentence = `%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?`
 const clenSentence = sentence.replace(/[^A-z\s]/gi, '').match(/([A-z])\w+|[I,a]/gi)
-//console.log(clenSentence)
+console.log(clenSentence)
 
 function threeMostFrequentWords(sentence ,num){
     const count = {}
