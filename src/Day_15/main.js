a01d3ff3bacd3f78204a597b02709a23161d84b6
hakan <hknfrt@protonmail.com>
2022-10-15 14:15:44 +0300

    day 15

--- src/Day_15/main.js
+++ src/Day_15/main.js
@@ -1,1 +1,15 @@
 // Day 15
+
+// Exercises Level 1
+
+
+// 1- Create an Animal class. The class will have name, age, color, legs properties and create different methods
+
+class Animal {
+    constructor(name, age, color, legs){
+        console.log(this)
+    }
+    
+}
+
+// Create a Dog and Cat child class from the Animal Class.
