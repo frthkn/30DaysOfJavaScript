f388dd45c6570d90a64e53515c66322f61c2d82f
hakan <hknfrt@protonmail.com>
2022-10-15 19:52:08 +0300

    day 15

--- src/Day_15/main.js
+++ src/Day_15/main.js
@@ -78,4 +78,60 @@ const goat1 = new Goat('Amy', 3, 'blue', 4, 'Abaza')
 
 // 1- Let's try to develop a program which calculate measure of central tendency of a sample(mean, median, mode) and measure of variability(range, variance, standard deviation). In addition to those measures find the min, max, count, percentile, and frequency distribution of the sample. You can create a class called Statistics and create all the functions which do statistical calculations as method for the Statistics class. Check the output below.
 
+const ages = [31, 26, 34, 37, 27, 26, 32, 32, 26, 27, 27, 24, 32, 33, 27, 25, 26, 38, 37, 31, 34, 24, 33, 29, 26]
+
+class Statistics {
+    constructor(count, sum, min, max, range, mean, median, mode,variance, standard_deviation,frequency_distribution){
+        this.count = count
+        this.sum =  sum
+        this.min = min
+        this.max = max
+        this.range = range 
+        this.mean = mean
+        this.median = median
+        this.mode = mode
+        this.variance = variance
+        this.standard_deviation = standard_deviation
+        this.frequency_distribution = frequency_distribution
+
+    }
+
+    static getValues(){
+        const count = ages.length
+        const sum =  ages.reduce((h,f)=> h+f)
+        const min = Math.min(...ages)
+        const max = Math.max(...ages)
+        const range = max-min
+        const mean = Math.ceil(sum/count)
+        const median = ages[Math.floor((ages.sort((k,l)=> k-l).length)/2)]
+        const modeFind = ages.reduce((el, i) => {const cnt = el[i] = el[i] ?? 0; return {...el, [i]: cnt +1};},{})
+        const mode = Object.entries(modeFind).sort((h,f)=> f[1]-h[1])
+        const varianceSqrt = ages.map((item => Math.abs(item-(Math.round(sum/count)))*Math.abs(item-(Math.round(sum/count)))))
+        const variance = varianceSqrt.reduce((m,n)=> (m+n))/varianceSqrt.length
+        const standard_deviation = Math.sqrt(variance).toFixed(1)
+        const percetForFreqDis = [...Object.values(mode).flatMap((i=> (i[1]*100)/25))]
+        const frequencyDistribution = [...Object.entries(mode).flatMap((i=> parseInt(i[1])))]
+        const frequency_distribution = []
+        for (const key in percetForFreqDis,frequencyDistribution) {
+            frequency_distribution.push([percetForFreqDis[key],frequencyDistribution[key]])
+        }
+        
+        return {
+            count, sum, min, max, range, mean, median, mode,variance, standard_deviation,frequency_distribution
+        }
+    }
+}
+
+//console.log(Statistics.getValues())
+
+
 // 2- Create a class called PersonAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+
+class PersonAccount {
+    constructor(firstName, lastName, incomes, expenses){
+        this.firstName = firstName
+        this.lastName = lastName
+        this.incomes = incomes
+        this.expenses = expenses
+    }
+}
