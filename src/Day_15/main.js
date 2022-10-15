2ee8d4f7345703289f926ee878549fbb70b75782
hakan <hknfrt@protonmail.com>
2022-10-15 15:08:16 +0300

    day 15

--- src/Day_15/main.js
+++ src/Day_15/main.js
@@ -53,7 +53,7 @@ const cat1 = new Cat('Sally',2,'black',4)
 // console.log(cat1)
 // console.log(cat1.saySomething())
 
-// Exercises Level 1
+// Exercises Level 2
 
 // 1- Override the method you create in Animal class
 
@@ -73,3 +73,9 @@ class Goat extends Animal {
 const goat1 = new Goat('Amy', 3, 'blue', 4, 'Abaza')
 // console.log(goat1)
 // console.log(goat1.saySomething())
+
+// Exercises Level 3
+
+// 1- Let's try to develop a program which calculate measure of central tendency of a sample(mean, median, mode) and measure of variability(range, variance, standard deviation). In addition to those measures find the min, max, count, percentile, and frequency distribution of the sample. You can create a class called Statistics and create all the functions which do statistical calculations as method for the Statistics class. Check the output below.
+
+// 2- Create a class called PersonAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
