07ad5e7251a92058d10ddc776131cf07d93d2740
hakan <hknfrt@protonmail.com>
2022-10-17 16:32:39 +0300

    day 16

--- src/Day_16/main.js
+++ src/Day_16/main.js
@@ -1,3 +1,9 @@
 // Day 15
 
 // Exercises Level 1
+
+
+// Change skills array to JSON using JSON.stringify()
+// Stringify the age variable
+// Stringify the isMarried variable
+// Stringify the student object
