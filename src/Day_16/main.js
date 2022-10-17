f110c106bc0efc8a06e7b19806771cd48898f722
hakan <hknfrt@protonmail.com>
2022-10-17 16:33:41 +0300

    day 16

--- src/Day_16/main.js
+++ src/Day_16/main.js
@@ -8,8 +8,14 @@
 // Stringify the isMarried variable
 // Stringify the student object
 
+
 // Exercises Level 2
 
+// Stringify the students object with only firstName, lastName and skills properties
 
 
 // Exercises Level 3
+
+
+// Parse the txt JSON to object.
+// Find the user who has many skills from the variable stored in txt.
