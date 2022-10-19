2b0e4d978b633122a292a6957fe73a2b3d0f95dd
hakan <hknfrt@protonmail.com>
2022-10-19 19:33:42 +0300

    day 17

--- src/Day_17/main.js
+++ src/Day_17/main.js
@@ -5,16 +5,29 @@
 // 1- Store you first name, last name, age, country, city in your browser localStorage.
 // You can assing these variables on your browser 
 
-localStorage.setItem('firstName', 'Hakan'),
-localStorage.setItem('lastName', 'Fırat'),
-localStorage.setItem('age', 34),
-localStorage.setItem('country', 'Turkey'),
-localStorage.setItem('city', 'Ankara')
+// localStorage.setItem('firstName', 'Hakan'),
+// localStorage.setItem('lastName', 'Fırat'),
+// localStorage.setItem('age', 34),
+// localStorage.setItem('country', 'Turkey'),
+// localStorage.setItem('city', 'Ankara')
 
 // Exercises: Level 2
 
 // Create a student object. The student object will have first name, last name, age, skills, country, enrolled keys and values for the keys. Store the student object in your browser localStorage.
 
+const student = {
+    firstName : 'Hakan',
+    lastName : 'Fırat',
+    age: 34,
+    country : 'Turkey',
+    skills : ['HTML', 'CSS', 'JS'],
+}
+console.log(student)
+localStorage.setItem('student',JSON.stringify(student))
+let studentParse = JSON.parse(localStorage.getItem('student'), undefined,4)
+console.log(studentParse)
+
 // Exercises: Level 3
 
-// Create an object called personAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+// 1- Create an object called personAccount. It has firstname, lastname, incomes, expenses properties and it has totalIncome, totalExpense, accountInfo,addIncome, addExpense and accountBalance methods. Incomes is a set of incomes and its description and expenses is also a set of expenses and its description.
+
