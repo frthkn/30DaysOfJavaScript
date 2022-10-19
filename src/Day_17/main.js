b10dc52a9447f88a351416dd1b53f5397a546be7
hakan <hknfrt@protonmail.com>
2022-10-19 11:07:54 +0300

    day 17

--- src/Day_17/main.js
+++ src/Day_17/main.js
@@ -5,3 +5,5 @@
 // 1- Store you first name, last name, age, country, city in your browser localStorage.
 
 // Exercises: Level 2
+
+// Create a student object. The student object will have first name, last name, age, skills, country, enrolled keys and values for the keys. Store the student object in your browser localStorage.
