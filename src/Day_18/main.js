ffaac80022a144af8670576e40e6243f6948eed4
hakan <hknfrt@protonmail.com>
2022-10-20 09:16:26 +0300

    day 18

--- src/Day_18/main.js
+++ src/Day_18/main.js
@@ -1,8 +1,39 @@
 // Day 18
 
+const countriesAPI = 'https://restcountries.com/v2/all'
+const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+
 // Exercises: Level 1
 
-// Read the countries API using fetch and print the name of country, capital, languages, population and area.
+// 1- Read the countries API using fetch and print the name of country, capital, languages, population and area.
+
+const url = 'https://restcountries.com/v2/all'
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const dataArr = []
+    for (const key in data) {
+       dataArr.push(['name',data[key].name, 'capital',data[key].capital, 'languages',data[key].languages, 'population',data[key].population, 'area',data[key].area] )
+    }
+    console.log(dataArr)
+  })
+  .catch(error => console.error(error))
+
+const fetchData = async () => {
+    try {
+      const response = await fetch(countriesAPI)
+      const countries = await response.json()
+      const countriesArr = []
+        for (const key in countries) {
+            countriesArr.push(['name',countries[key].name, 'capital',countries[key].capital, 'languages',countries[key].languages, 'population',countries[key].population, 'area',countries[key].area] )
+        }
+        console.log(countriesArr)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  // console.log('async and await')
+  // fetchData()
 
 
 // Exercises: Level 2
