35816783c08d6c741b9b43691101982113e9eb99
hakan <hknfrt@protonmail.com>
2022-10-21 12:36:06 +0300

    day 20

---
+++ src/Day_20/main.js
@@ -0,0 +1,146 @@
+// Day 20
+
+// Variables
+
+
+let firstName = 'Hakan'
+let lastName = 'Fırat'
+let country = 'Turkey'
+let city = 'Ankara'
+
+const PI = Math.PI
+const gravity = 9.81
+
+// Arrays
+
+// Chose to make array names plural
+
+
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook', 'Hakan']
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100]
+
+
+
+// Functions
+
+// function which return full name of a person
+const printFullName = (firstName, lastName) => firstName + ' ' + lastName
+
+// function which calculates a square of a number
+const square = (n) => n * n
+
+// a function which generate random hexa colors
+const hexaColor = () => {
+  const str = '0123456789abcdef'
+  let hexa = '#'
+  let index
+  for (let i = 0; i < 6; i++) {
+    index = Math.floor(Math.random() * str.length)
+    hexa += str[index]
+  }
+  return hexa
+}
+
+// a function which shows date and time
+const showDateTime = () => {
+  const now = new Date()
+  const year = now.getFullYear()
+  const month = now.getMonth() + 1
+  const date = now.getDate()
+  let hours = now.getHours()
+  let minutes = now.getMinutes()
+  if (hours < 10) {
+    hours = '0' + hours
+  }
+  if (minutes < 10) {
+    minutes = '0' + minutes
+  }
+
+  const dateMonthYear = date + '.' + month + '.' + year
+  const time = hours + ':' + minutes
+  const fullTime = dateMonthYear + ' ' + time
+  return fullTime
+}
+
+// The new Dat().toLocaleString() can also be used to display current date time. The toLocaleString() methods takes different arguments. 
+
+// Loops
+
+for (let i = 0; i < n; i++){
+    console.log()
+}
+
+// iterating an array using regular for loop
+let len = names.length;
+for(let i = 0; i < len; i++){
+    console.log(names[i].toUpperCase())
+}
+
+
+// iterating an array using for of
+for( const name of names) {
+    console.log(name.toUpperCase())
+}
+
+// iterating array using forEach 
+names.forEach((name) => name.toUpperCase())
+
+
+const person = {
+  firstName: 'Hakan',
+  lastName: 'Fırat',
+  age: 34,
+  country: 'Turkey',
+  city: 'Ankara',
+  skills: ['HTML','CSS','JavaScript'],
+  isMarried: true
+}
+for(const key in person) {
+    console.log(key)
+}
+
+
+// if else if else if else
+
+let a = 0
+if (a > 0) {
+ console.log(`${a} is a positive number`)
+} else if (a < 0) {
+ console.log(`${a} is a negative number`)
+} else if (a == 0) {
+ console.log(`${a} is zero`)
+} else {
+ console.log(`${a} is not a number`)
+}
+
+// Switch More Examples
+let dayUserInput = prompt('What day is today ?')
+let day = dayUserInput.toLowerCase()
+
+switch (day) {
+ case 'monday':
+   console.log('Today is Monday')
+   break
+ case 'tuesday':
+   console.log('Today is Tuesday')
+   break
+ case 'wednesday':
+   console.log('Today is Wednesday')
+   break
+ case 'thursday':
+   console.log('Today is Thursday')
+   break
+ case 'friday':
+   console.log('Today is Friday')
+   break
+ case 'saturday':
+   console.log('Today is Saturday')
+   break
+ case 'sunday':
+   console.log('Today is Sunday')
+   break
+ default:
+   console.log('It is not a week day.')
+}
+
+
