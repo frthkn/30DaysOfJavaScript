6b18d0fb15cec76e97f1d3cd21be0aa678c06951
hakan <hknfrt@protonmail.com>
2022-10-24 13:48:30 +0300

    day 21

--- src/Day_21/main.js
+++ src/Day_21/main.js
@@ -36,9 +36,12 @@ const setId = setAttributeToAllPTag.forEach((i => i.setAttribute('id', 'newID'))
 // console.log(setAttributeToAllPTag)
 
 
-
 // Exercise: Level 2
 
+
+// Change stye of each paragraph using JavaScript(eg. color, background, border, font-size, font-family)
+// Select all paragraphs and loop through each elements and give the first and third paragraph a color of green, and the second and the fourth paragraph a red color
+// Set text content, id and class to each paragraph
 
 
 // Exercise: Level 3
