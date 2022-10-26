4b345cb8a5fc6825fa8bb3f5cb309e36be784a36
hakan <hknfrt@protonmail.com>
2022-10-26 08:46:38 +0300

    day 22

--- src/Day_22/main1.js
+++ src/Day_22/main1.js
@@ -44,7 +44,7 @@ document.querySelectorAll('.nums').forEach((el=> el.style.backgroundColor = 'whi
 document.querySelectorAll('.nums').forEach((el=> el.style.border = 'solid'))
 document.querySelectorAll('.nums').forEach((el=> el.style.padding = '35px'))
 document.querySelectorAll('.nums').forEach((el,i)=> {
-    if(i == 2 || i == 3 || i == 5 || i == 7 || i == 11 || i == 13 || i == 17 || i == 19 || i == 23 || i == 29 || i == 31 || i == 37 || i == 41 || i == 43 || i == 47 || i == 53 || i == 59 || i == 61 || i == 67 || i == 71 || i == 73 || i == 79 || i == 83 || i == 89 || i == 97){
+    if(i == 2 || i == 3 || i == 5 || i == 7 || i == 11 || i == 13 || i == 17 || i == 19 || i == 23 || i == 29 || i == 31 || i == 37 || i == 41 || i == 43 || i == 47 || i == 53 || i == 59 || i == 61 || i == 67 || i == 71 || i == 73 || i == 79 || i == 83 || i == 89 || i == 97 || i == 101){
         el.style.backgroundColor ='red'
     }else if(i%2 == 0){
         el.style.backgroundColor ='green'
