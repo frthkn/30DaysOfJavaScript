1fd5456b38c64536100cbf28776de1fd82d66c56
hakan <hknfrt@protonmail.com>
2022-11-03 12:14:18 +0300

    day 23

--- src/Day_23/main2.js
+++ src/Day_23/main2.js
@@ -1,11 +1,3 @@
 // Day 23
 
 // Exercise 2
-// document.querySelector('h1').style.textAlign ='center'
-// const p = document.querySelector('p')
-// document.body.addEventListener('keypress', e => {
-//    p.innerHTML = 'You press'+e.code
-   
-// })
-
-document.write('hello there')
