dd73390de46a0397acbf4af3efda60440bfbf95a
hakan <hknfrt@protonmail.com>
2022-11-03 14:48:17 +0300

    day 23

--- src/Day_23/main2.js
+++ src/Day_23/main2.js
@@ -5,8 +5,9 @@
 
 const body = document.querySelector('body')
 
-
-h1 = document.addEventListener('keydown' ,e => {
+const label = document.querySelector('.showKeyCode')
+label.textContent = 'Press any keyboard key'
+label = document.addEventListener('keydown' ,e => {
     e.preventDefault()
     h1.textContent = 'You pressed '+e.key 
     span.textContent = e.code.codePointAt()
