7443b2fa3feebf7947d865aff427b2815d5df79e
hakan <hknfrt@protonmail.com>
2022-11-03 20:04:24 +0300

    day 23

--- src/Day_23/main2.js
+++ src/Day_23/main2.js
@@ -6,6 +6,7 @@ const mainDiv = document.querySelector('div')
 const divKey = document.querySelector('#key')
 const span = document.querySelector('span')
 const divKeyCode = document.createElement('div')
+const labelKeyCode =  document.createElement('label')
 divKeyCode.style.width = '200px'
 divKeyCode.style.height ='200px'
 divKeyCode.style.display = 'flex'
@@ -16,13 +17,16 @@ divKeyCode.style.fontSize='50px'
 
 
 
+
 divKey = document.addEventListener('keydown' ,e => {
     divKey.textContent = 'You pressed '
     e.preventDefault()
     span.innerHTML = e.key[0].toUpperCase()+e.key.slice(1)
     divKey.appendChild(span)
     span.style.color = 'green'
-    divKeyCode.textContent = e.code.charCodeAt()
+    labelKeyCode.textContent = e.code.charCodeAt()
+    labelKeyCode.style.color ='green'
+    divKeyCode.appendChild(labelKeyCode)
     mainDiv.appendChild(divKeyCode)
 
     
