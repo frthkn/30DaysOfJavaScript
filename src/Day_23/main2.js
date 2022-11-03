d95863f9d855bd5334babcc0f13fb89c98f9bcb6
hakan <hknfrt@protonmail.com>
2022-11-03 17:52:16 +0300

    day 23

--- src/Day_23/main2.js
+++ src/Day_23/main2.js
@@ -7,11 +7,14 @@ const divKey = document.querySelector('#key')
 const divKeyCode = document.querySelector('#keyCode')
 const span = document.querySelector('span')
 
-divKey.textContent = 'You pressed '
 
-document.addEventListener('keydown' ,e => {
+
+divKey = document.addEventListener('keydown' ,e => {
+    divKey.textContent = 'You pressed '
     e.preventDefault()
-    
-    span.textContent = e.key
+    span.innerHTML = e.key[0].toUpperCase()+e.key.slice(1)
+    divKey.appendChild(span)
+    span.style.color = 'green'
+    divKeyCode.textContent = e.code.charCodeAt()
     
 })
