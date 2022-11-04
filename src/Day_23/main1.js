5d23e67d3dd5094bfb61bfa629771ea0fffe26d2
hakan <hknfrt@protonmail.com>
2022-11-04 23:45:49 +0300

    day 23

--- src/Day_23/main1.js
+++ src/Day_23/main1.js
@@ -3,10 +3,10 @@
 // Number Generator Exercise
 
 const p = document.querySelector('p')
-document.querySelector('input').addEventListener('blur',(e) =>{
-   p.textContent = 'Enter number value on the input field to generate numbers'
-   p.style.color = 'red'
-   
-
+const inputa = document.querySelector('input')
+const button = document.querySelector('button')
+p.style.color = 'red'
+inputa.addEventListener('input',(e) =>{
+   p.textContent = e.target.value
 })
 
