6a85c14118cdc340c69a4de4a1ed903d200c6a84
hakan <hknfrt@protonmail.com>
2022-11-04 12:00:04 +0300

    day 23

--- src/Day_23/main1.js
+++ src/Day_23/main1.js
@@ -3,10 +3,9 @@
 // Number Generator Exercise
 
 const div  = document.querySelector('div')
-
-const input = document.querySelector('input').addEventListener('keydown',e =>{
-    if(typeof e.key !='number'){
-        const p = document.createElement('p')
-        p.textContent = 'input value must be number'
+const label = document.querySelector('.enterNum')
+const input = document.querySelector('input').addEventListener('blur',e =>{
+    if(e.target ==='string'){
+        label.innerText = 'input value must be number'
     }
 })
