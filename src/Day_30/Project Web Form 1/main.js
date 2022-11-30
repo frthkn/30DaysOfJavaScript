29bf8bff3d9a462fcb4cb71b25eaf056977c8e15
Hakan FIRAT <113293688+frthkn@users.noreply.github.com>
2022-11-30 21:22:12 +0300

    Day 30 part 1

---
+++ src/Day_30/Project Web Form 1/main.js
@@ -0,0 +1,23 @@
+// Day 30 Web Form
+
+
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+
+        let forms = document.getElementsByClassName('needs-validation');
+
+        let validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                    
+                
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
+
